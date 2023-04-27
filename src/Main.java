0918fc515acd8d21776f94940e9213007f9bcb15
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-27 01:18:39 -0300

    Created classes Course and Mentorship and created class objects in the main method

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descricao curso Js");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria Java");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
+
+    }
+}
